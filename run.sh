e1cf0349249a0ba4035ebc9bb4ff1e23bcbd431b
Александр Орешкин <oreshkin.dev@outlook.com>
2024-06-07 20:37:57 +0300

    Инициализация раннера

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+. ./env.sh
+
+
+# Run the Go program with data race detection enabled and the CGO_ENABLED
+# environment variable set to 1. This is done to enable the data race
+# detection feature provided by the Go runtime.
+#
+# Args:
+#   None
+
+# Remove the data race detection feature on production builds
+
+go run -race *.go
