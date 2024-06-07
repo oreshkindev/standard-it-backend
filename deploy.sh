57e9cfc4f916fd1d1487290063109a9c186203db
Александр Орешкин <oreshkin.dev@outlook.com>
2024-06-07 20:45:01 +0300

    Скрипт для работы со сборкой, выгрузкой и запуском сервиса

---
+++ deploy.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+. ./env.sh
+
+# This script is used to deploy the binary file to a server and
+# restart the corresponding service.
+#
+# Args:
+#   None
+
+# Build the binary
+GOOS=linux GOARCH=amd64 go build -o bin/main cmd/*.go
+
+# Deploy the binary file from local machine to the server
+rsync --archive --compress --delete bin/main ${SSH_URL}:${PATH}/bin/
+
+# Restart the service on the server
+ssh "${SSH_URL}" "
+    sudo systemctl restart ${SERVICE_NAME}
+"
+
+# Inform the user that the script has finished executing
+if [ $? -eq 0 ]; then
+    echo "Binary has been deployed successfully."
+else
+    echo "Binary deployment failed."
+    exit 1
+fi
